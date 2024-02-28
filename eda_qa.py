06354d8c3fbe1b1b6a0ba861fad370dbe1366528
Its-EdgarS <edgar.s7223@gmail.com>
2024-02-28 01:23:28 -0500

    added function that lists questions and answers from a json

--- eda_qa.py
+++ eda_qa.py
@@ -1,6 +1,24 @@
 import json
 
+
+def printQA(file):
+    data = json.load(open(file, encoding='utf8'))
+    qas = list()
+
+    with open('Questions_and_Answers.txt', 'w', encoding='utf8') as f:
+        for item in data['data']:
+            for pg in item['paragraphs']:
+                for qa in pg['qas']:
+                    f.write("Question: " + qa['question'] + "\n")
+                    f.write("Answer: " + qa['answers'][0]['text'] + "\n\n")
+                    qas.append((qa['question'], qa['answers'][0]['text']))
+    return qas
+
 questions = json.load(open('train-v1.1.json', encoding='utf8'))
 
 with open('clean_train.json', 'w') as newfile:
     json.dump(questions, newfile, indent=2)
+
+file = "clean_train.json"
+qas = printQA(file)
+print(qas)
