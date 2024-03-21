975b823dd1d58e215478380068b9dd136c0823b2
Eddie Kubit <eddiekubit0@gmail.com>
2024-03-21 17:02:50 -0400

    created QA json

--- eda_qa.py
+++ eda_qa.py
@@ -5,13 +5,21 @@ def printQA(file):
     data = json.load(open(file, encoding='utf8'))
     qas = list()
 
-    with open('Questions_and_Answers.txt', 'w', encoding='utf8') as f:
-        for item in data['data']:
-            for pg in item['paragraphs']:
-                for qa in pg['qas']:
-                    f.write("Question: " + qa['question'] + "\n")
-                    f.write("Answer: " + qa['answers'][0]['text'] + "\n\n")
-                    qas.append((qa['question'], qa['answers'][0]['text']))
+    for item in data['data']:
+        for pg in item['paragraphs']:
+            for qa in pg['qas']:
+                curr = dict()
+                curr['question'] = qa['question']
+                curr['answer'] = qa['answers'][0]['text']
+                curr['humor'] = False
+                qas.append(curr)
+                    # f.write("Question: " + qa['question'] + "\n")
+                    # f.write("Answer: " + qa['answers'][0]['text'] + "\n\n")
+                    # qas.append((qa['question'], qa['answers'][0]['text']))
+                
+    with open('Questions_and_Answers.json', 'w', encoding='utf8') as f:
+        json.dump(qas, f, indent=4)
+
     return qas
 
 questions = json.load(open('train-v1.1.json', encoding='utf8'))
@@ -21,4 +29,4 @@ with open('clean_train.json', 'w') as newfile:
 
 file = "clean_train.json"
 qas = printQA(file)
-print(qas)
+#print(qas)
