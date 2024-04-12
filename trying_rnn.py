979c0e833efcaeaf1b54cecb557a7e77996c430a
ncyranek <90420768+ncyranek@users.noreply.github.com>
2024-04-12 11:56:04 -0400

    Create trying_rnn.py
    Going to play with this for a bit

---
+++ trying_rnn.py
@@ -0,0 +1,9 @@
+import os
+import time
+import load_data
+import torch
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torch.optim as optim
+import numpy as np
+from models.LSTM import LSTMClassifier
